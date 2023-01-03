6cb7fdea5cf0e23ed601e1e15609d4b585844fec
Efuan_Simms <flaziblaz@hotmail.com>
2023-01-03 10:06:32 -0600

    test 3
    test

--- production/Tables/TestTable.sql
+++ production/Tables/TestTable.sql
@@ -2,5 +2,6 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY identity(1,1), 
     [Testcol] VARCHAR(50) NOT NULL, 
-    [Testcol2] NCHAR(10) NULL
+    [Testcol2] NCHAR(10) NULL, 
+    [Testcol3] NCHAR(10) NULL
 )
