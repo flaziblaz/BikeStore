1eff0faa6b8079dd742a5351b3e7e004b95f8caf
Efuan_Simms <flaziblaz@hotmail.com>
2023-01-02 19:50:23 -0600

    test 2

--- production/Tables/TestTable.sql
+++ production/Tables/TestTable.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[TestTable]
 (
 	[Id] INT NOT NULL PRIMARY KEY identity(1,1), 
-    [Testcol] VARCHAR(50) NOT NULL
+    [Testcol] VARCHAR(50) NOT NULL, 
+    [Testcol2] NCHAR(10) NULL
 )
