770734305a358b27c60144de3bdbf40da9b52db1
Efuan_Simms <flaziblaz@hotmail.com>
2023-01-06 16:50:35 -0600

    88
    88

--- production/Tables/Warehouse.sql
+++ production/Tables/Warehouse.sql
@@ -3,5 +3,6 @@
 	[Id] INT identity(1,1) NOT NULL PRIMARY KEY, 
     [WarehouseName] VARCHAR(50) NOT NULL, 
     [WarehouseDescription] VARCHAR(100) NULL,
+    [Test] NCHAR(10) NULL, 
     constraint uc_warehousename unique (WarehouseName)
 )
